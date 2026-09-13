ac4ced2d054cd48d5f2e224148839cebeaf7dd1a
Yaroslava Nazarova <ulicadom96@gmail.com>
2026-09-13 18:11:08 +0300

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Hello, my name is Yaroslava, and I study at IU5 13/4!" << endl;
+    cout << "Updated message!" << endl;
     return 0;
 }
